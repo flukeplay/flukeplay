c47fdc99035aa21ac994f1941c4803d3434a9d4d
flukeplay <84685761+flukeplay@users.noreply.github.com>
2021-05-26 15:49:04 +0300

    Create FlukeTokenHPB.sol

---
+++ contracts/FlukeTokenHPB.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.6;
+
+import "./FlukeToken.sol";
+
+contract FlukeTokenHPB is FlukeToken{
+
+    constructor() public FlukeToken("Fluke HPB", "FLKHPB"){
+        _minPlayCoinForBonus = 10**18; //1 HPB
+    }
+
+    function getSecureRandomNumberSeed() internal view returns(bytes32){
+        //return block.random;
+        revert("block.random not enabled");
+    }
+
+}
