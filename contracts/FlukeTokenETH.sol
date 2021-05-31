8834955afa635d98a5a4f9c5087e3779b5ad74ad
flukeplay <84685761+flukeplay@users.noreply.github.com>
2021-05-31 08:51:55 +0300

    Update FlukeTokenETH.sol

--- contracts/FlukeTokenETH.sol
+++ contracts/FlukeTokenETH.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.5.6;
+pragma solidity ^0.5.7;
 
 import "./FlukeToken.sol";
 
 contract FlukeTokenETH is FlukeToken{
 
     constructor() public FlukeToken("Fluke ETH", "FLKETH"){
-        _minPlayCoinForBonus = 2*10**15; //0.00025 ETH
+        _minPlayCoinsForBonus = 2*10**15; //0.00025 ETH
     }
 }
