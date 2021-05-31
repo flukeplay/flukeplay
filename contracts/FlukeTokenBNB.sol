680fe55cdfb1dbc1bdca44bfa8e2427eba7717d3
flukeplay <84685761+flukeplay@users.noreply.github.com>
2021-05-31 08:51:26 +0300

    Update FlukeTokenBNB.sol

--- contracts/FlukeTokenBNB.sol
+++ contracts/FlukeTokenBNB.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.5.6;
+pragma solidity ^0.5.7;
 
 import "./FlukeToken.sol";
 
 contract FlukeTokenBNB is FlukeToken{
 
     constructor() public FlukeToken("Fluke BNB", "FLKBNB"){
-        _minPlayCoinForBonus = 2*10**15; //0.002 BNB
+        _minPlayCoinsForBonus = 2*10**15; //0.002 BNB
     }
 }
